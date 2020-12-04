d8ed7d73eb860cf1da6b2ba14898c065294bb7bd
OksanaKulbaba <o.coolbaba@gmail.com>
2020-12-04 16:40:28 +0200

    Add some config

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const config = require('config')
 const mongoose = require('mongoose')
 const upload = require('./middleware/upload')
+const path = require('path')
 
 
 const app = express()
@@ -11,6 +12,14 @@ app.use(express.json({extended : true}))
 app.use('/api/film/load', upload.single('file'), require('./routes/loadfile.router'))
 app.use('/api/film', require('./routes/film.routes'))
 app.use('/api/actor', require('./routes/actors.routers'))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+      
+    });
+}
 
 const PORT = config.get('port') || 5000
 
